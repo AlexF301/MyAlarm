7dbaab7717c8f533bb6d66c5d17690771ca36426
Alex <floressoaa@moravian.edu>
2022-11-30 12:09:23 -0500

    Fragments can be viewed through a viewpager

--- app/src/main/java/com/android/myalarm/MainActivity.kt
+++ app/src/main/java/com/android/myalarm/MainActivity.kt
@@ -2,13 +2,34 @@ package com.android.myalarm
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.android.myalarm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var viewpager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //TODO: alarm, timer, and stopwatch as a viewpager2?
+        viewpager = binding.viewPager2
+        setUpViewPager()
+    }
+
+    private fun setUpViewPager() {
+        val myAdapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        //add Fragments in your ViewPagerFragmentAdapter class
+        myAdapter.addFragment(AlarmsListFragment())
+        myAdapter.addFragment(TimerFragment())
+        myAdapter.addFragment(StopWatchFragment())
+
+        // set Orientation in your ViewPager2
+        viewpager.adapter = myAdapter
     }
 }
